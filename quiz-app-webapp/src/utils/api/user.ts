b0acab3f1f3237d0e16501c01f9120822eecc065
maksim.sheitser@foundevs.com <maksim.sheitser@foundevs.com>
2024-05-14 00:50:08 +0200

    feat: change password api

--- quiz-app-webapp/src/utils/api/user.ts
+++ quiz-app-webapp/src/utils/api/user.ts
@@ -1,6 +1,10 @@
 import { axiosInstance } from './axiosInstance';
-import { ProfileResponse } from '../dto/profile';
+import {ChangePasswordRequest, ProfileResponse} from '../dto/profile';
 
 export const getCurrentUser = (email: string) => {
   return axiosInstance.post<ProfileResponse>('/user', { email }).then((res) => res?.data);
+};
+export const changePassword = (values: ChangePasswordRequest) => {
+  return true;
+  return axiosInstance.post('/user/change-password', values).then((res) => res?.data);
 };
