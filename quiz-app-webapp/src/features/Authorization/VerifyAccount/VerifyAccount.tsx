9a413aec56c17f3139be233099f6006419034b95
maksim.sheitser@foundevs.com <maksim.sheitser@foundevs.com>
2024-05-13 23:31:18 +0200

    feat: authorization verify account

---
+++ quiz-app-webapp/src/features/Authorization/VerifyAccount/VerifyAccount.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import { URL_LOGIN } from "utils/constants/clientUrl";
+import { Button } from "antd";
+import { SubmitButton } from "components/SubmitButton";
+
+import './VerifyAccount.scss';
+
+export const VerifyAccount = () => {
+    const navigate = useNavigate();
+
+    const resendEmail = () => {}
+
+    const goLogin = () => navigate(URL_LOGIN.path());
+
+    return (
+        <div className="authPage">
+            <h2 className="authLabel">Verify Your Account</h2>
+
+            <div className="authPageVerifyText">
+                <p>A verification email has been sent to your email address</p>
+                <p>Please check your inbox and confirm your account by clicking the verification link</p>
+                <p>
+                    If you missed it  check your spam folder or click below to resend your verification email
+                </p>
+            </div>
+
+            <SubmitButton id="authSubmit" className="authButton" showSpinner onClick={resendEmail}>
+                Re-send Email
+            </SubmitButton>
+
+            <Button type="primary" className="authButton" onClick={goLogin}>Go to Login</Button>
+        </div>
+    );
+};
