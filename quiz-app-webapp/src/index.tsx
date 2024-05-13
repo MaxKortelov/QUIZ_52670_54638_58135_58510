2c99e7327c144da9c4ca1ab657c8cc2e5f9b8ee0
maksim.sheitser@foundevs.com <maksim.sheitser@foundevs.com>
2024-05-13 23:13:20 +0200

    feat: app index

--- quiz-app-webapp/src/index.tsx
+++ quiz-app-webapp/src/index.tsx
@@ -1,20 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-import store from './store/store'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "components/App";
+
+import "./index.scss";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
-);
+root.render(<App />);
