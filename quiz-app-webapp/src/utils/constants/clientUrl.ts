8a500c16fb6ac119320343395503c3ee4bc49cd5
maksim.sheitser@foundevs.com <maksim.sheitser@foundevs.com>
2024-05-13 23:17:42 +0200

    feat: constants (client urls, localstorage constants, viewportWidth)

---
+++ quiz-app-webapp/src/utils/constants/clientUrl.ts
@@ -0,0 +1,13 @@
+import { makeUrl } from '../router/makeUrl';
+
+export const URL_HOME = new makeUrl('/');
+export const URL_LOGIN = new makeUrl('/login');
+export const URL_SIGN_UP = new makeUrl('/sing-up');
+export const URL_VERIFY = new makeUrl('/verify-account');
+export const URL_FORBIDDEN = new makeUrl('/403');
+export const URL_NOT_FOUND = new makeUrl('/404');
+export const URL_CHANGE_PASSWORD = new makeUrl('/change-password');
+export const URL_SUPPORT = new makeUrl('/support');
+export const URL_NOTIFICATIONS = new makeUrl('/notifications');
+export const URL_QUIZ_INTRO = new makeUrl('/quiz/:id/intro');
+export const URL_QUIZ = new makeUrl('/quiz/:id');
---
+++ quiz-app-webapp/src/utils/constants/index.ts
@@ -0,0 +1,2 @@
+export const QUIZ_IS_LOGGED = 'QUIZ_IS_LOGGED';
+export const QUIZ_EMAIL = 'QUIZ_EMAIL';
