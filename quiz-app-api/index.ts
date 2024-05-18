43c426754e92368a07dc2fa03f46fd1e8020c910
Max <maksym@mac.local.ua>
2024-05-19 00:20:40 +0200

    modified env

--- quiz-app-api/index.ts
+++ quiz-app-api/index.ts
@@ -1,4 +1,9 @@
-import "dotenv/config";
+import path from "path";
+import dotenv from "dotenv";
 import {startServer} from "./server";
+
+const envFile = process.env.NODE_ENV === "prod" ? ".env" : ".env.development"
+
+dotenv.config({ path: path.resolve(__dirname, envFile) })
 
 startServer();
