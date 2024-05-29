d01529feb311479f0a387c40848dd7a225233396
Max <maksym@mac.local.ua>
2024-05-29 11:13:03 +0200

    removed comments

--- quiz-app-api/tests/jestGlobalSetup.ts
+++ quiz-app-api/tests/jestGlobalSetup.ts
@@ -1,10 +1,1 @@
-import db from "../db";
-
-module.exports = async function connectDB() {
-    // await db
-    //   .connect()
-    //   .then(() =>
-    //     console.info(`Connected to quiz database on port ${process.env.DB_PORT} successfully`)
-    //   )
-    //   .catch(() => console.error("Failed to connect to quiz database"));
-};
+module.exports = async function () {};
--- quiz-app-api/tests/jestGlobalTeardown.ts
+++ quiz-app-api/tests/jestGlobalTeardown.ts
@@ -1,5 +1,1 @@
-import db from "../db";
-
-module.exports = async function disconnectDb() {
-  // await db.end().then(() => console.info("DB connection ended"))
-};
+module.exports = async function () {};
