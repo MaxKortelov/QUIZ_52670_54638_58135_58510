f15f97ffbffd634eb32225573354c529d51cec05
Max <maksym@mac.local.ua>
2024-05-29 13:38:52 +0200

    added api util tests

--- quiz-app-api/tests/unit/utils/api.util.test.ts
+++ quiz-app-api/tests/unit/utils/api.util.test.ts
@@ -1,10 +1,9 @@
 import {responseMessage} from "../../../utils/api.util";
-import {ResponseMessage} from "../../../types/api.util";
 
 describe("Test api utils", () => {
   test("It should modify message correctly - responseMessage", () => {
     const message = "test";
 
-    expect(responseMessage(message)).toEqual({ message });
+    expect(responseMessage(message)).toStrictEqual({ message });
   })
 })
