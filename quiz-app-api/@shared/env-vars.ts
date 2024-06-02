f5f8bbeeda18956312d62604faaca9e275c0d2d9
Max <maksym@mac.local.ua>
2024-06-02 09:31:48 +0200

    fixed encryption

--- quiz-app-api/@shared/env-vars.ts
+++ quiz-app-api/@shared/env-vars.ts
@@ -12,7 +12,8 @@ export const databaseOptions: ClientConfig = {
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT)
+  port: Number(process.env.DB_PORT),
+  ssl: true
 };
 
 export const nodeMailerOptions = {
